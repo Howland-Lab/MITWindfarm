d8390e4bcbc497ed665b42b761a343491d9b3cee
Ilan <iupfal@mit.edu>
2024-07-02 09:02:36 -0400

    Make base_windfield and TIamb an input passed to Windfarm at call time for all Windfarm models

--- mitwindfarm/windfarm.py
+++ mitwindfarm/windfarm.py
@@ -11,7 +11,7 @@ from .Rotor import (
     AnalyticalAvgAD,
     AnalyticalAvgUnifiedAD,
     ReferenceRotor,
-    AnalyticalAvgReferenceRotor
+    AnalyticalAvgReferenceRotor,
 )
 from .Windfield import Windfield, Uniform
 from .Wake import WakeModel, Wake, GaussianWakeModel, VariableKwGaussianWakeModel
@@ -72,39 +72,52 @@ class Windfarm:
         rotor_model: Union[AD, UnifiedAD] = None,
         wake_model: Optional[WakeModel] = None,
         superposition: Optional[Superposition] = None,
-        base_windfield: Optional[Windfield] = None,
-        TIamb: float = None,
     ):
         self.rotor_model = AD() if rotor_model is None else rotor_model
         self.wake_model = GaussianWakeModel() if wake_model is None else wake_model
         self.superposition = Linear() if superposition is None else superposition
-        self.base_windfield = (
-            Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
-        )
-        self.TIamb = TIamb
 
     def __call__(
-        self, layout: Layout, setpoints: list[tuple[float, ...]]
+        self,
+        layout: Layout,
+        setpoints: list[tuple[float, ...]],
+        base_windfield: Optional[Windfield] = None,
+        TIamb: float = None,
     ) -> WindfarmSolution:
+        
+        base_windfield = (
+            Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
+        )
+
         N = layout.x.size
         wakes = N * [None]
         rotor_solutions = N * [None]
 
-        windfield = self.superposition(self.base_windfield, [])
+        windfield = self.superposition(base_windfield, [])
         for i, (x, y, z) in layout.iter_downstream():
             rotor_solutions[i] = self.rotor_model(x, y, z, windfield, *setpoints[i])
             rotor_solutions[i].idx = i
-            wakes[i] = self.wake_model(x, y, z, rotor_solutions[i], TIamb=self.TIamb)
+            wakes[i] = self.wake_model(x, y, z, rotor_solutions[i], TIamb=TIamb)
             windfield.add_wake(wakes[i])
 
         return WindfarmSolution(layout, setpoints, rotor_solutions, wakes, windfield)
 
-    def from_partial(self, partial: PartialWindfarmSolution) -> WindfarmSolution:
+    def from_partial(
+        self,
+        partial: PartialWindfarmSolution,
+        base_windfield: Optional[Windfield] = None,
+        TIamb: float = None,
+    ) -> WindfarmSolution:
+
+        base_windfield = (
+            Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
+        )
+
         N = len(partial.layout)
         wakes = N * [None]
-        windfield = self.superposition(self.base_windfield, [])
+        windfield = self.superposition(base_windfield, [])
         for i, (x, y, z) in partial.layout.iter_downstream():
-            wakes[i] = self.wake_model(x, y, z, partial.rotors[i])
+            wakes[i] = self.wake_model(x, y, z, partial.rotors[i], TIamb=TIamb)
             windfield.add_wake(wakes[i])
 
         return WindfarmSolution(
@@ -121,20 +134,19 @@ class ReferenceWindfarm:
         rotor_model: Union[ReferenceRotor] = None,
         wake_model: Optional[WakeModel] = None,
         superposition: Optional[Superposition] = None,
-        base_windfield: Optional[Windfield] = None,
-        TIamb: float = None,
     ):
         self.rotor_model = ReferenceRotor() if rotor_model is None else rotor_model
         self.wake_model = GaussianWakeModel() if wake_model is None else wake_model
         self.superposition = Linear() if superposition is None else superposition
-        self.base_windfield = (
-            Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
-        )
-        self.TIamb = TIamb
 
     def __call__(
-        self, layout: Layout, thrust_spts: list[float], yaw_spts: list[float],
-        urated: float
+        self,
+        layout: Layout,
+        thrust_spts: list[float],
+        yaw_spts: list[float],
+        urated: float,
+        base_windfield: Optional[Windfield] = None,
+        TIamb: float = None,
     ) -> WindfarmSolution:
         """
         Parameters:
@@ -142,113 +154,12 @@ class ReferenceWindfarm:
             - thrust_spts: Ct' setpoints of each turbine or list of None of
                 length of turbines, if setpoint is None, setpoint will be
                 determined using ThrustCurve.
-            - urated: the rated wind speed of the reference turbine used 
+            - urated: the rated wind speed of the reference turbine used
                 normalized by the free stream wind speed.
 
         Returns:
             - a WindfarmSolution object normalized by the free stream velocity.
         """
-        N = layout.x.size
-        wakes = N * [None]
-        rotor_solutions = N * [None]
-        setpoints = N * [None]
-
-        windfield = self.superposition(self.base_windfield, [])
-        for i, (x, y, z) in layout.iter_downstream():
-            rotor_solutions[i] = self.rotor_model(
-                x, y, z, windfield, thrust_spts[i], yaw_spts[i], urated
-            )
-            rotor_solutions[i].idx = i
-            wakes[i] = self.wake_model(x, y, z, rotor_solutions[i], TIamb=self.TIamb)
-            windfield.add_wake(wakes[i])
-            setpoints[i] = (rotor_solutions[i].Ctprime, rotor_solutions[i].yaw)
-
-        return WindfarmSolution(layout, setpoints, rotor_solutions, wakes, windfield)
-
-    def from_partial(self, partial: PartialWindfarmSolution) -> WindfarmSolution:
-        N = len(partial.layout)
-        wakes = N * [None]
-        windfield = self.superposition(self.base_windfield, [])
-        for i, (x, y, z) in partial.layout.iter_downstream():
-            wakes[i] = self.wake_model(x, y, z, partial.rotors[i])
-            windfield.add_wake(wakes[i])
-
-        return WindfarmSolution(
-            partial.layout, partial.setpoints, partial.rotors, wakes, windfield
-        )
-
-    def from_dict(self, partial: dict) -> WindfarmSolution:
-        return self.from_partial(PartialWindfarmSolution.from_dict(partial))
-
-
-class AnalyticalAvgWindfarm:
-    def __init__(
-        self,
-        rotor_model: Union[AnalyticalAvgAD, AnalyticalAvgUnifiedAD] = None,
-        wake_model: Union[GaussianWakeModel, VariableKwGaussianWakeModel] = None,
-        base_windfield: Optional[Windfield] = None,
-        TIamb: float = None,
-    ):
-        self.rotor_model = AnalyticalAvgAD() if rotor_model is None else rotor_model
-        self.wake_model = GaussianWakeModel() if wake_model is None else wake_model
-        self.superposition = Linear()
-        self.base_windfield = (
-            Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
-        )
-        self.TIamb = TIamb
-
-    def __call__(
-        self, layout: Layout, setpoints: list[tuple[float, ...]]
-    ) -> WindfarmSolution:
-        N = layout.x.size
-        wakes = N * [None]
-        rotor_solutions = N * [None]
-
-        windfield = self.superposition(self.base_windfield, [])
-        for i, (x, y, z) in layout.iter_downstream():
-            rotor_solutions[i] = self.rotor_model(x, y, z, windfield, *setpoints[i])
-            rotor_solutions[i].idx = i
-            wakes[i] = self.wake_model(x, y, z, rotor_solutions[i], TIamb=self.TIamb)
-            windfield.add_wake(wakes[i])
-
-        return WindfarmSolution(layout, setpoints, rotor_solutions, wakes, windfield)
-
-    def from_partial(self, partial: PartialWindfarmSolution) -> WindfarmSolution:
-        N = len(partial.layout)
-        wakes = N * [None]
-        windfield = self.superposition(self.base_windfield, [])
-        for i, (x, y, z) in partial.layout.iter_downstream():
-            wakes[i] = self.wake_model(x, y, z, partial.rotors[i])
-            windfield.add_wake(wakes[i])
-
-        return WindfarmSolution(
-            partial.layout, partial.setpoints, partial.rotors, wakes, windfield
-        )
-
-    def from_dict(self, partial: dict) -> WindfarmSolution:
-        return self.from_partial(PartialWindfarmSolution.from_dict(partial))
-
-
-class AnalyticalAvgReferenceWindfarm:
-    def __init__(
-        self,
-        rotor_model: Optional[AnalyticalAvgReferenceRotor] = None,
-        wake_model: Union[GaussianWakeModel, VariableKwGaussianWakeModel] = None,
-        
-        
-    ):
-        self.rotor_model = (
-            AnalyticalAvgReferenceRotor() if rotor_model is None else rotor_model
-        )
-        self.wake_model = GaussianWakeModel() if wake_model is None else wake_model
-        self.superposition = Linear()
-        
-
-    def __call__(
-        self, layout: Layout, thrust_spts: list[float], yaw_spts: list[float],
-        urated: float, base_windfield: Optional[Windfield] = None, TIamb: float = None
-    ) -> WindfarmSolution:
-        
         base_windfield = (
             Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
         )
@@ -270,14 +181,147 @@ class AnalyticalAvgReferenceWindfarm:
 
         return WindfarmSolution(layout, setpoints, rotor_solutions, wakes, windfield)
 
-    def from_partial(self, partial: PartialWindfarmSolution,
-                     base_windfield: Optional[Windfield] = None, TIamb: float = None
-                     ) -> WindfarmSolution:
+    def from_partial(
+        self,
+        partial: PartialWindfarmSolution,
+        base_windfield: Optional[Windfield] = None,
+        TIamb: float = None,
+    ) -> WindfarmSolution:
+
+        base_windfield = (
+            Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
+        )
+
+        N = len(partial.layout)
+        wakes = N * [None]
+        windfield = self.superposition(base_windfield, [])
+        for i, (x, y, z) in partial.layout.iter_downstream():
+            wakes[i] = self.wake_model(x, y, z, partial.rotors[i], TIamb=TIamb)
+            windfield.add_wake(wakes[i])
+
+        return WindfarmSolution(
+            partial.layout, partial.setpoints, partial.rotors, wakes, windfield
+        )
+
+    def from_dict(self, partial: dict) -> WindfarmSolution:
+        return self.from_partial(PartialWindfarmSolution.from_dict(partial))
+
+
+class AnalyticalAvgWindfarm:
+    def __init__(
+        self,
+        rotor_model: Union[AnalyticalAvgAD, AnalyticalAvgUnifiedAD] = None,
+        wake_model: Union[GaussianWakeModel, VariableKwGaussianWakeModel] = None,
+    ):
+        self.rotor_model = AnalyticalAvgAD() if rotor_model is None else rotor_model
+        self.wake_model = GaussianWakeModel() if wake_model is None else wake_model
+        self.superposition = Linear()
+
+    def __call__(
+        self,
+        layout: Layout,
+        setpoints: list[tuple[float, ...]],
+        base_windfield: Optional[Windfield] = None,
+        TIamb: float = None,
+    ) -> WindfarmSolution:
         
         base_windfield = (
             Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
         )
         
+        N = layout.x.size
+        wakes = N * [None]
+        rotor_solutions = N * [None]
+
+        windfield = self.superposition(base_windfield, [])
+        for i, (x, y, z) in layout.iter_downstream():
+            rotor_solutions[i] = self.rotor_model(x, y, z, windfield, *setpoints[i])
+            rotor_solutions[i].idx = i
+            wakes[i] = self.wake_model(x, y, z, rotor_solutions[i], TIamb=TIamb)
+            windfield.add_wake(wakes[i])
+
+        return WindfarmSolution(layout, setpoints, rotor_solutions, wakes, windfield)
+
+    def from_partial(
+        self,
+        partial: PartialWindfarmSolution,
+        base_windfield: Optional[Windfield] = None,
+        TIamb: float = None,
+    ) -> WindfarmSolution:
+
+        base_windfield = (
+            Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
+        )
+
+        N = len(partial.layout)
+        wakes = N * [None]
+        windfield = self.superposition(base_windfield, [])
+        for i, (x, y, z) in partial.layout.iter_downstream():
+            wakes[i] = self.wake_model(x, y, z, partial.rotors[i], TIamb=TIamb)
+            windfield.add_wake(wakes[i])
+
+        return WindfarmSolution(
+            partial.layout, partial.setpoints, partial.rotors, wakes, windfield
+        )
+
+    def from_dict(self, partial: dict) -> WindfarmSolution:
+        return self.from_partial(PartialWindfarmSolution.from_dict(partial))
+
+
+class AnalyticalAvgReferenceWindfarm:
+    def __init__(
+        self,
+        rotor_model: Optional[AnalyticalAvgReferenceRotor] = None,
+        wake_model: Union[GaussianWakeModel, VariableKwGaussianWakeModel] = None,
+    ):
+        self.rotor_model = (
+            AnalyticalAvgReferenceRotor() if rotor_model is None else rotor_model
+        )
+        self.wake_model = GaussianWakeModel() if wake_model is None else wake_model
+        self.superposition = Linear()
+
+    def __call__(
+        self,
+        layout: Layout,
+        thrust_spts: list[float],
+        yaw_spts: list[float],
+        urated: float,
+        base_windfield: Optional[Windfield] = None,
+        TIamb: float = None,
+    ) -> WindfarmSolution:
+
+        base_windfield = (
+            Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
+        )
+
+        N = layout.x.size
+        wakes = N * [None]
+        rotor_solutions = N * [None]
+        setpoints = N * [None]
+
+        windfield = self.superposition(base_windfield, [])
+        for i, (x, y, z) in layout.iter_downstream():
+            rotor_solutions[i] = self.rotor_model(
+                x, y, z, windfield, thrust_spts[i], yaw_spts[i], urated
+            )
+            rotor_solutions[i].idx = i
+            wakes[i] = self.wake_model(x, y, z, rotor_solutions[i], TIamb=TIamb)
+            windfield.add_wake(wakes[i])
+            setpoints[i] = (rotor_solutions[i].Ctprime, rotor_solutions[i].yaw)
+
+        return WindfarmSolution(layout, setpoints, rotor_solutions, wakes, windfield)
+
+    def from_partial(
+        self,
+        partial: PartialWindfarmSolution,
+        base_windfield: Optional[Windfield] = None,
+        TIamb: float = None,
+    ) -> WindfarmSolution:
+
+        base_windfield = (
+            Uniform(TIamb=TIamb) if base_windfield is None else base_windfield
+        )
+
         N = len(partial.layout)
         wakes = N * [None]
         windfield = self.superposition(base_windfield, [])
