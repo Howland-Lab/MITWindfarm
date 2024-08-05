727b5bca8a345b5ab6757e9f3e7e12b4e38d07c8
iupfal <iupfal@mit.edu>
2024-08-05 10:44:57 -0400

    add to __init__

--- mitwindfarm/__init__.py
+++ mitwindfarm/__init__.py
@@ -1,7 +1,7 @@
 from ._Layout import GridLayout, Square, Layout
-from .Rotor import RotorSolution, AD, UnifiedAD, BEM, AnalyticalAD, AnalyticalUnifiedAD, FixedControlAD
+from .Rotor import RotorSolution, AD, UnifiedAD, BEM, AnalyticalAD, AnalyticalUnifiedAD, FixedControlAD, FixedControlAnalyticalAD
 from .RotorGrid import Point, Line, Area
 from .Superposition import Linear, Quadratic, Dominant
 from .Wake import WakeModel, GaussianWakeModel, GaussianWake, VariableKwGaussianWakeModel
-from .windfarm import WindfarmSolution, PartialWindfarmSolution, Windfarm, AnalyticalWindfarm, FixedControlWindfarm
+from .windfarm import WindfarmSolution, PartialWindfarmSolution, Windfarm, AnalyticalWindfarm, FixedControlWindfarm, FixedControlAnalyticalWindfarm
 from .Windfield import Uniform, PowerLaw, Superimposed
