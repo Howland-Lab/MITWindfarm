2a8157b0535aa9870f7b5976d5acc7c5c694ce58
iupfal <iupfal@mit.edu>
2024-08-07 15:12:27 -0400

    Nondimensionalize SetpointCurve object.

--- mitwindfarm/SetpointCurve.py
+++ mitwindfarm/SetpointCurve.py
@@ -21,20 +21,18 @@ class SetpointCurve():
     Thrust setpoint curve for an actuator disk operating at individual control optimum.
 
     """
-    def __init__(self, rated: float = 10.59, cutout:float=25.0):
+    def __init__(self):
         self.AD_model = Heck()
-        self.rated_ws = rated
-        self.cutout = cutout
-        self.windspeeds = np.linspace(2, cutout, 200) / rated
+        self.windspeeds = np.linspace(0.01, 3.0, 2000)
 
-        def get_optimal_power(windspeed):
+        def get_optimal_power(ws):
             
             def f(x):
                 return - self.AD_model(x, 0.0).Cp
 
             def constraint_func(x):
                 Cp = self.AD_model(x, 0.0).Cp[0]
-                Cp_max = ((16/27) * (1 / windspeed) ** 3)
+                Cp_max = ((16/27) * (1 / ws) ** 3)
                 return Cp_max - Cp
 
             constraint = dict(type="ineq", fun=constraint_func)
