774708006e6e91623e8303c251d141d8d2f23e7d
iupfal <iupfal@mit.edu>
2024-11-11 17:21:21 -0500

    add Niayifar superposition

--- mitwindfarm/Superposition.py
+++ mitwindfarm/Superposition.py
@@ -14,7 +14,10 @@ class Linear(Superposition):
     def __call__(self, base_windfield: Windfield, wakes: list[Wake]) -> Windfield:
         return Superimposed(base_windfield, wakes, method="linear")
 
-
+class Niayifar(Superposition):
+    def __call__(self, base_windfield: Windfield, wakes: list[Wake]) -> Windfield:
+        return Superimposed(base_windfield, wakes, method="niayifar")
+    
 class Quadratic(Superposition):
     def __call__(self, base_windfield: Windfield, wakes: list[Wake]) -> Windfield:
         return Superimposed(base_windfield, wakes, method="quadratic")
