12b08140cb38202c9cc23a617af4c44e4a7341a9
Skylar Gering <sgering@mit.edu>
2025-10-20 13:34:30 -0400

    Update plotting to allow any slice

--- mitwindfarm/Plotting.py
+++ mitwindfarm/Plotting.py
@@ -4,37 +4,62 @@ import numpy as np
 from .windfarm import WindfarmSolution
 
 
-def plot_windfarm(sol: WindfarmSolution, ax=None, pad=1, z=None, frame=True, axis=False, res: int=400):
+def plot_windfarm(sol: WindfarmSolution, ax=None, pad=1, x=None, y=None, z=None, frame=True, axis=False, res: int=400):
+    if sum(val is not None for val in (x, y, z)) > 1:
+        raise ValueError("At most one of x, y, or z can br provided to plot a 2D slice of the wind farm")
+
     if ax is None:
         _, ax = plt.subplots()
-
     xlim = (np.min(sol.layout.x) - pad, np.max(sol.layout.x) + 10)
     ylim = (np.min(sol.layout.y) - pad, np.max(sol.layout.y) + pad)
-    z = np.mean(sol.layout.z) if z is None else z
-
-    # plot windfield and turbine stats
-    _x, _y = np.linspace(*xlim, res), np.linspace(*ylim, res)
-    xmesh, ymesh = np.meshgrid(_x, _y)
-
-    wsp = sol.windfield.wsp(xmesh, ymesh, np.full_like(xmesh, z))
-    ax.imshow(
-        wsp, extent=[*xlim, *ylim], vmin=0, vmax=2, origin="lower", cmap="RdYlBu_r"
-    )
-
-    for (turb_x, turb_y, _), rotor in zip(sol.layout, sol.rotors):
-        # Draw turbine
-        R = 0.5
-        p = np.array([[0, 0], [+R, -R]])
-        rotmat = np.array(
-            [
-                [np.cos(rotor.yaw), -np.sin(rotor.yaw)],
-                [np.sin(rotor.yaw), np.cos(rotor.yaw)],
-            ]
+    zlim = (np.min(sol.layout.z) - pad, np.max(sol.layout.z) + pad)
+    xz_frame = False
+    yz_frame = False
+    if y is not None:
+        xz_frame = True
+        _x, _z = np.linspace(*xlim, res), np.linspace(*zlim, res)
+        xmesh, zmesh = np.meshgrid(_x, _z)
+        wsp = sol.windfield.wsp(xmesh, np.full_like(xmesh, y), zmesh)
+        ax.imshow(
+            wsp, extent=[*xlim, *zlim], vmin=0, vmax=2, origin="lower", cmap="RdYlBu_r"
         )
-
-        p = rotmat @ p + np.array([[turb_x], [turb_y]])
-
-        ax.plot(p[0, :], p[1, :], "k", lw=2)
+    elif x is not None:
+        yz_frame = True
+        _y, _z = np.linspace(*ylim, res), np.linspace(*zlim, res)
+        ymesh, zmesh = np.meshgrid(_y, _z)
+        wsp = sol.windfield.wsp(np.full_like(ymesh, x), ymesh, zmesh)
+        ax.imshow(
+            wsp, extent=[*ylim, *zlim], vmin=0, vmax=2, origin="lower", cmap="RdYlBu_r"
+        )
+    else:
+        z = np.mean(sol.layout.z) if z is None else z
+        _x, _y = np.linspace(*xlim, res), np.linspace(*ylim, res)
+        xmesh, ymesh = np.meshgrid(_x, _y)
+        wsp = sol.windfield.wsp(xmesh, ymesh, np.full_like(xmesh, z))
+        ax.imshow(
+            wsp, extent=[*xlim, *ylim], vmin=0, vmax=2, origin="lower", cmap="RdYlBu_r"
+        )
+     
+    for (turb_x, turb_y, turb_z), rotor in zip(sol.layout, sol.rotors):
+        # Draw turbine
+        if yz_frame:
+            if turb_x == x:
+                a, b = [turb_y], [turb_z]
+                ax.scatter([turb_y], [turb_z], c = "k")
+        else:
+            R = 0.5
+            p = np.array([[0, 0], [+R, -R]])
+            alpha = -rotor.tilt if xz_frame else rotor.yaw
+            rotmat = np.array(
+                [
+                    [np.cos(alpha), -np.sin(alpha)],
+                    [np.sin(alpha), np.cos(alpha)],
+                ]
+            )
+            trans_vec = [[turb_x], [turb_z]] if xz_frame else [[turb_x], [turb_y]]
+            p = rotmat @ p + np.array(trans_vec)
+            # add in line for turbine
+            ax.plot(p[0, :], p[1, :], "k", lw=2)
     ax.set(frame_on=frame)
     if axis is False:
         ax.set(xticks=[], yticks=[])
